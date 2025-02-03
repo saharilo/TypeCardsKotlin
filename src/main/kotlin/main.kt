46e191084a313b4298bea745cfef73ffadb1b03e
Sah test <sahar-ilya@mail.ru>
2025-02-03 23:21:00 +0700

    Function CommissionCard added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,21 @@
 package ru.netology
 
 fun main () {
-    
+    val result = commissionCard("Mastercard", 50000, 30000)
+    println("Комиссия за перевод составит $result рублей.")
+}
+
+fun commissionCard(typeCard: String = "Мир", sumMonthTransfer: Int = 0, transfer: Int): Int {
+   return when (typeCard) {
+       "Мир" -> 0
+       "Visa" -> {
+           val result = (transfer * 0.0075).toInt()
+           if (result < 35) 35 else result
+       }
+       "Mastercard" -> {
+           val upLimit = sumMonthTransfer + transfer - 75000
+           if (upLimit <= 0) 0 else (upLimit * 0.006 + 20).toInt()
+       }
+       else -> -1
+    }
 }
